690ca045c8564ed838343bcdd1d8460302ec1f4e
Sara-Albagir <saraaalbagiir@gmail.com>
2023-11-30 07:36:03 -0800

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    arg = input("Enter sth: ")
-    arg = arg.split()
-    count = len(arg)
+    import sys
+
+    count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments")
     elif count == 1:
-        print("{} argument:".format(count))
-        print("1: {}".format(arg[0]))
+        print("1 argument:")
     else:
         print("{} arguments:".format(count))
-        for i in range(count):
-            print("{}: {}".format(i + 1, arg[i]))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
